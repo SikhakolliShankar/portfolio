40d60b543e27ddd6904416d5f4c83014557eadf6
Shankar <sikhakollishankar16@gmail.com>
2024-07-14 15:21:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname) {
+            for (tablink of tablinks) {
+                tablink.classList.remove("active-link");
+            }
+
+            for (tabcontent of tabcontents) {
+                tabcontent.classList.remove("active-tab");
+            }
+
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab");
+        }
+
+
+
+
+        // JavaScript to toggle menu visibility
+        
+        function toggleMenu() {
+            const navMenu = document.getElementById('nav-menu');
+            const menuDisplay = navMenu.style.display;
+        
+            console.log(menuDisplay);
+        
+            if (menuDisplay === 'none' || menuDisplay === '') {
+                navMenu.style.display = 'flex';
+            } else {
+                navMenu.style.display = 'none';
+            }
+        }
+        
